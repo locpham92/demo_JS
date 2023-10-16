2c6d3f7ff5a2764d14a420ad5f7deb242c0f04e8
locpham92 <phamquyloc92@gmail.com>
2023-10-16 16:35:19 +0700

    JS demo

---
+++ demoJS.js
@@ -0,0 +1,12 @@
+function showMessage() {
+    alert('Xin chào!');
+}
+    function showMessage2(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+    function showMessage3(){
+    document.write('Xin chào!');
+}
+    function showMessage4(){
+    console.log('Xin chào!');
+}
